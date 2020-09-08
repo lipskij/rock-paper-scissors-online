6fa6e6c537df92f174d23347edb4790a6ddd4abf
lipskij <emil.lipskij@gmail.com>
2020-09-08 22:13:19 +0300

    fix create game logic

--- imports/api/games.js
+++ imports/api/games.js
@@ -13,23 +13,32 @@ export const GamesCollection = new Mongo.Collection("games");
 
 Meteor.methods({
   CreateGame(username) {
-    console.log("here");
-    const result = GamesCollection.upsert(
-      {
-        players: {
-          $size: 1,
-        },
-      },
-      { $addToSet: { players: username } }
-    );
+    //
+    // upsert is not returning updated ObjectId
+    // find() games with the single player in them !!!
+    // use insert() if size === 0
+    // use update if size === 1
+    // return gameID
 
-    return { gameID: result.insertedId };
+    const games = GamesCollection.find({
+      players: {
+        $size: 1,
+      },
+    });
+    if (games.count() === 0) {
+      const gameID = GamesCollection.insert({ players: [username] });
+      return { gameID };
+    }
+    const existingGame = games.fetch()[0];
+    GamesCollection.update(existingGame._id, {
+      $addToSet: { players: username },
+    });
+    return { gameID: existingGame._id };
   },
 
   Choice(payload) {
     const game = GamesCollection.findOne(payload.gameID);
-    console.log(game);
-    console.log(payload);
+    // compare : gameID username and choice
     return "choice";
   },
 });
