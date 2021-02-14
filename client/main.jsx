90f07bd501a7ebe49c49161b38f541e3234f86b3
Emil Lipskij <emil.lipskij@gmail.com>
2021-02-15 00:02:04 +0200

    prettier

--- client/main.jsx
+++ client/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import { App } from '/imports/ui/App';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+import { App } from "/imports/ui/App";
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  render(<App />, document.getElementById("react-target"));
 });
 
 Meteor.startup(() => {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register("/sw.js")
     .then()
-    .catch((error) => console.log('ServiceWorker registration failed: ', err));
+    .catch((error) => console.log("ServiceWorker registration failed: ", err));
 });
